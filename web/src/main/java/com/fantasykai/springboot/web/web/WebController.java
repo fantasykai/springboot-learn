970c1273dbdaa184c510cea28bb75695c33b33d1
makai <kai.fantasy@gmail.com>
2018-03-07 15:59:59 +0800

    拆分为多个模块- 快速了解SpringBoot Web 开发-参数传递

--- web/src/main/java/com/fantasykai/springboot/web/web/WebController.java
+++ web/src/main/java/com/fantasykai/springboot/web/web/WebController.java
@@ -1,9 +1,13 @@
 package com.fantasykai.springboot.web.web;
 
 import com.fantasykai.springboot.web.domian.User;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 〈一句话功能简述〉<br>
@@ -18,6 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebController {
 
 
+    /**
+     * Gets user.
+     *
+     * @return the user
+     */
     @RequestMapping(value = "/oneUser", method = RequestMethod.GET)
     public User getUser() {
 
@@ -29,5 +38,50 @@ public class WebController {
         return user;
     }
 
+    /**
+     * Gets users.
+     *
+     * @return the users
+     */
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public List<User> getUsers() {
+        List<User> users = new ArrayList<User>();
+
+        User user1 = new User();
+        user1.setName("小黑");
+        user1.setSex(1);
+        user1.setAge(18);
+
+        User user2 = new User();
+        user2.setName("小红");
+        user2.setSex(2);
+        user2.setAge(19);
+
+        User user3 = new User();
+        user2.setName("小白");
+        user2.setSex(1);
+        user2.setAge(20);
+
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+
+        return users;
+    }
+
+    /**
+     * Gets user by name.
+     *
+     * @param name the name
+     * @return the user by name
+     */
+    @RequestMapping(value = "/user/{name}", method = RequestMethod.GET)
+    public User getUserByName(@PathVariable String name) {
+        User user = new User();
+        user.setName(name);
+        user.setAge(16);
+        user.setSex(1);
+        return user;
+    }
 
 }
--- web/src/test/java/com/fantasykai/springboot/web/WebApplicationTests.java
+++ web/src/test/java/com/fantasykai/springboot/web/WebApplicationTests.java
@@ -11,22 +11,52 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+/**
+ * The type Web application tests.
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebApplicationTests {
 
     private MockMvc mockMvc;
 
+    /**
+     * Sets up.
+     */
     @Before
     public void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(new WebController()).build();
     }
 
+    /**
+     * Gets user.
+     *
+     * @throws Exception the exception
+     */
     @Test
     public void getUser() throws Exception {
-
         mockMvc.perform(MockMvcRequestBuilders.get("/oneUser")).andDo(MockMvcResultHandlers.print());
+    }
 
+    /**
+     * Gets users.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void getUsers() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users")).andDo(MockMvcResultHandlers.print());
+    }
+
+
+    /**
+     * Gets user by name.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void getUserByName() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/fantasy")).andDo(MockMvcResultHandlers.print());
     }
 
 }
