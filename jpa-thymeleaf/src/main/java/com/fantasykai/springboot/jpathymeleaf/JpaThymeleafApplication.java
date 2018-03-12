4d3d7bcb6984afedaf5991e91814493ca1b9dfcd
makai <kai.fantasy@gmail.com>
2018-03-12 16:08:59 +0800

    SpringBoot JPA + Thymeleaf demo 及总结

---
+++ jpa-thymeleaf/src/main/java/com/fantasykai/springboot/jpathymeleaf/JpaThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.fantasykai.springboot.jpathymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaThymeleafApplication.class, args);
+	}
+}
